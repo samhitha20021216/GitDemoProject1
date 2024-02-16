6da2d96b66d5e7946da28502cc4468de8fbb4572
smbanda <smbanda@247GKL3.corp.copart.com>
2024-02-16 11:20:58 +0530

    First commit

---
+++ src/GIT/Test.java
@@ -0,0 +1,15 @@
+package GIT;
+
+public class Test {
+
+	public static void name(String[] args) {
+
+		String h="yes";
+		if(h=="yes") {
+			System.out.println("go eat");
+		}
+		else {
+			System.out.println(" go study");
+		}
+	}
+}
